d0bd6c66e05b7606863f1c6da6a499629660438b
NovaShen555 <admin@novashen.top>
2024-04-25 00:15:06 +0800

    delete some file reconstitute some redirection move data to /static/utils/data

--- WeatherViewer/urls.py
+++ WeatherViewer/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.jumptohome, name='jumptohome'),
     path('home/', views.home, name='home'),
-    path('bulletins/', views.jumptobulletins, name='jumptobulletins'),
     path('search/', views.search_results, name='search_results'),
     path('guidance/',views.search_ex,name='examples'),
     path('api/weathermap',views.weather_map,name='wmap'),
     path('map/',views.weather_report,name='performap'),
     path('bulletins/<msg_index>',views.bulletins,name='bulletins'),
+    path('bulletins/', views.bulletins, name='jumptobulletins'),
     path('graphs/',views.graphs,name='graphs'),
     path('alarm/',views.alarm_map,name='alarm'),
     path('weather/<city_index>',views.get_city_weather,name='get_city_weather')
