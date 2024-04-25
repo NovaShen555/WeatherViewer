bb2371ef4bb6fb71e69801c99f356b40318e86b5
pjh123 <116361784+pjh456@users.noreply.github.com>
2024-04-25 08:49:24 +0800

    0.4.25.8.50
    模型还没写完，中午把这些先定义完了再说

--- WeatherViewer/models.py
+++ WeatherViewer/models.py
@@ -1,10 +1,43 @@
 from django.db import models
-
-class CityInfo(models.Model):
-    cityname = models.CharField(max_length=10)
-    cityurl = models.URLField()
-    WeatherUpdateDate = models.DateField()
     
+# 用于保存一起更新的东西的更新时间
+# 比如地图上的标记，还有天气数值排行那里
 class UpdateTime(models.Model):
     timeid = models.CharField(max_length=50)
     deltatime = models.DateField()
+    
+# 地图上城市天气标记
+class WeatherSignData(models.Model):
+    type = models.IntegerField() # 行政级别
+    url = models.CharField(max_length=5) # 后五位url
+    city = models.CharField(max_length=10) # 城市名
+    postid = models.CharField(max_length=6) # 邮政编码
+    belongs = models.CharField(max_length=5) # 所属的市级辖区代码 比如ANM
+    
+    # 高低温 
+    high_temp = models.IntegerField()
+    low_temp = models.IntegerField()
+    # 早上
+    m_weather_name = models.CharField(max_length=10) # 天气名
+    m_weather_img = models.CharField(max_length=3) # 对应的图片编号
+    m_wind_direction = models.CharField(max_length=5) # 风向
+    m_wind_power = models.CharField(max_length=5) # 风力
+    # 晚上
+    w_weather_name = models.CharField(max_length=10) # 天气名
+    w_weather_img = models.CharField(max_length=3) # 对应的图片编号
+    w_wind_direction = models.CharField(max_length=5) # 风向
+    w_wind_power = models.CharField(max_length=5) # 风力
+    
+# 预警标记
+class AlarmData(models.Model):
+    id = models.CharField(max_length=50) # 对应的编号
+    headline = models.CharField(max_length=50) # 头条（不知道干啥用的）
+    time = models.CharField(max_length=20) # 发生时间
+    description = models.TextField() # 具体描述
+    longitude = models.FloatField() # 经度
+    latitude = models.FloatField() # 纬度
+    img = models.CharField(max_length=20) # 图片编号
+    title = models.CharField(max_length=50) # 标题
+    
+# 还有公告消息，气象图，城市具体天气数值没做，我中午回来搞
+    
